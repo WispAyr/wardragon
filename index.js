f21ab090442263db2ad4a84f3a925df7c55ceb67
Ewan Richardson <ewan@wispayr.online>
2024-03-24 18:54:03 +0000

    update server port from config

--- index.js
+++ index.js
@@ -279,7 +279,7 @@ process.on('unhandledRejection', (reason, promise) => {
     console.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || config.ServerPort;
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
